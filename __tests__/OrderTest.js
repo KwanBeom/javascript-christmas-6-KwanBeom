d2cc4242b182dc6ca5a47f75c5a9e16004c91495
BK <3031011@naver.com>
2023-11-12 16:57:00 +0900

    refactor: 테스트 코드 리팩토링
    테스트 코드 BDD 패턴 적용

--- __tests__/OrderTest.js
+++ __tests__/OrderTest.js
@@ -1,56 +1,53 @@
-/* eslint-disable max-lines-per-function */
 import Order from '../src/models/Order';
 
-describe('주문 테스트', () => {
-  test('메뉴판에 없는 메뉴를 주문하면 예외가 발생한다.', () => {
+describe('메뉴 주문 시', () => {
+  it('메뉴판에 없는 메뉴를 주문하면 예외가 발생한다.', () => {
     const order = new Order();
-    expect(() => {
-      order.addOrder([['등갈비스테이크', '1']]);
-    }).toThrow('[ERROR]');
+
+    // Given
+    const menu = [['등갈비스테이크', '1']];
+
+    // When & Then
+    expect(() => order.addOrder(menu)).toThrow('[ERROR]');
   });
 
-  test('제대로 입력한 케이스, 예외가 발생하지 않는다.', () => {
+  it('제대로 입력한 케이스, 예외가 발생하지 않는다.', () => {
     const order = new Order();
-    expect(() => {
-      order.addOrder([
-        ['티본스테이크', '1'],
-        ['양송이수프', '2'],
-        ['제로콜라', '1'],
-      ]);
-    }).not.toThrow('[ERROR]');
+    const menu = [
+      ['티본스테이크', '1'],
+      ['양송이수프', '2'],
+      ['제로콜라', '1'],
+    ];
+    expect(() => order.addOrder(menu)).not.toThrow('[ERROR]');
   });
 
-  test('주문 수량이 올바르지 않으면 예외가 발생한다.', () => {
+  it('주문 수량이 올바르지 않으면 예외가 발생한다.', () => {
     const order = new Order();
-    expect(() => {
-      order.addOrder([['양송이수프', '0']]);
-    }).toThrow('[ERROR]');
+    const menu = [['양송이수프', '0']];
+    expect(() => order.addOrder(menu)).toThrow('[ERROR]');
   });
 
-  test('중복 메뉴를 주문한 경우 예외가 발생한다.', () => {
+  it('중복 메뉴를 주문한 경우 예외가 발생한다.', () => {
     const order = new Order();
-    expect(() => {
-      order.addOrder([
-        ['양송이수프', '1'],
-        ['양송이수프', '1'],
-      ]);
-    }).toThrow('[ERROR]');
+    const menu = [
+      ['양송이수프', '1'],
+      ['양송이수프', '1'],
+    ];
+    expect(() => order.addOrder(menu)).toThrow('[ERROR]');
   });
 
-  test('메뉴를 20개 이상 주문한 경우 예외가 발생한다.', () => {
+  it('메뉴를 20개 이상 주문한 경우 예외가 발생한다.', () => {
     const order = new Order();
-    expect(() => {
-      order.addOrder([
-        ['양송이수프', '10'],
-        ['제로콜라', '15'],
-      ]);
-    }).toThrow('[ERROR]');
+    const menu = [
+      ['양송이수프', '10'],
+      ['제로콜라', '15'],
+    ];
+    expect(() => order.addOrder(menu)).toThrow('[ERROR]');
   });
 
-  test('음료만 주문한 경우 예외가 발생한다.', () => {
+  it('음료만 주문한 경우 예외가 발생한다.', () => {
     const order = new Order();
-    expect(() => {
-      order.addOrder([['제로콜라', '1']]);
-    }).toThrow('[ERROR]');
+    const menu = [['제로콜라', '1']];
+    expect(() => order.addOrder(menu)).toThrow('[ERROR]');
   });
 });
