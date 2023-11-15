fdd51ecba239cd960cf9b2d5216cf5a5c15c80ce
BK <3031011@naver.com>
2023-11-15 19:33:06 +0900

    refactor: OutputView 구조 및 코드 개선

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,87 +1,76 @@
 import { Console } from '@woowacourse/mission-utils';
 import GIFT from '../constants/gift';
 import MENU from '../constants/menu';
-import Membership from '../models/Membership';
 import { TITLE } from '../constants/message';
+import Membership from '../models/Membership';
+import formatter from '../utils/formatter';
 
 const OutputView = {
-  numberFormat(number) {
-    return new Intl.NumberFormat().format(number);
-  },
-  viewOrderDetail(totalPrice, orderDetail) {
-    return `
-      ${TITLE.ORDERED_MENU}
-      ${Array.from(orderDetail)
-        .map(([menu, quantity]) => `${menu} ${quantity}개`)
-        .join('\n')
-      }
-
-      ${TITLE.TOTAL_PRICE}
-      ${this.numberFormat(totalPrice)}원
-    `;
-  },
-  benefitDetail(benefit, visitDate) {
+  discountDetail(benefit, visitDate) {
+    const dDayDiscount = benefit.dDayDiscountPrice();
+    const weekDiscount = benefit.weekDiscountPrice();
+    const specialDiscount = benefit.specialDiscountPrice();
+    const isWeekday = visitDate.isWeekday();
     const discountDetails = [
-      this.dDayDiscountDetail(benefit.dDayDiscountPrice()),
-      this.weekDiscountDetail(benefit.weekDiscountPrice(), visitDate),
-      this.specialDiscountDetail(benefit.specialDiscountPrice()),
-    ]
-      .filter(Boolean)
+      dDayDiscount && `크리스마스 디데이 할인: -${formatter.numberFormat(dDayDiscount)}원`,
+      specialDiscount && `특별 할인: -${formatter.numberFormat(specialDiscount)}원`,
+      weekDiscount && `${isWeekday ? '평일' : '주말'} 할인 : -${formatter.numberFormat(weekDiscount)}원`,
+    ].filter(Boolean)
       .join('\n');
 
-    return `${discountDetails || '없음'}`;
+    return `${discountDetails.length > 0 ? discountDetails : '없음'}`;
   },
-  dDayDiscountDetail(dDayDiscountPrice) {
-    if (!dDayDiscountPrice) {
-      return null;
-    }
-    return `크리스마스 디데이 할인: -${this.numberFormat(dDayDiscountPrice)}원`;
 
-  },
-  weekDiscountDetail(weekDiscountPrice, visitDate) {
-    if (!weekDiscountPrice) {
-      return null;
-    }
-
-    return `${visitDate.isWeekday() ? '평일' : '주말'} 할인 : -${this.numberFormat(
-      weekDiscountPrice,
-    )}원`;
-  },
-  specialDiscountDetail(specialDiscountPrice) {
-    if (!specialDiscountPrice) {
-      return null;
-    }
-    return `특별 할인: -${this.numberFormat(specialDiscountPrice)}원`;
-  },
-  viewBenefits(benefit, visitDate) {
-    const giftData = Object.values(MENU)
-      .flat()
-      .find((menu) => menu.key === GIFT.DECEMBER.KEY) || {};
+  giftDetail(benefit) {
+    const giftData =
+      Object.values(MENU)
+        .flat()
+        .find((menu) => menu.key === GIFT.DECEMBER.KEY) || {};
     const giftEventPrice = benefit.giftEventPrice();
 
-    return `${TITLE.GIFT_MENU}
-      ${giftEventPrice ? `${giftData.name} ${giftEventPrice / giftData.price}개` : '없음'} 
-
-      ${TITLE.BENEFIT_DETAIL}
-      ${this.benefitDetail(benefit, visitDate)}
-      
-      ${TITLE.BENEFIT_AMOUNT}
-      ${this.numberFormat(benefit.totalBenefitPrice() + benefit.giftEventPrice())}원`;
+    return `${giftEventPrice ? `${giftData.name} ${giftEventPrice / giftData.price}개` : '없음'}`;
   },
-  createRecipt(totalPrice, orderDetail, visitDate, benefit) {
+
+  viewBenefitDetail(benefit, visitDate) {
+    const giftDetail = this.giftDetail(benefit);
+    const discountDetail = this.discountDetail(benefit, visitDate);
+    const benefitAmount = benefit.totalBenefitPrice() + benefit.giftEventPrice();
+
+    return [
+      TITLE.GIFT_MENU,
+      giftDetail,
+      TITLE.BENEFIT_DETAIL,
+      discountDetail,
+      TITLE.BENEFIT_AMOUNT,
+      `${formatter.numberFormat(benefitAmount)}원`,
+    ].join('\n');
+  },
+
+  viewOrderDetail(totalPrice, orders) {
     return `
-      ${this.viewOrderDetail(totalPrice, orderDetail)}
+      ${TITLE.ORDERED_MENU}
+      ${Array.from(orders)
+        .map(([menu, quantity]) => `${menu} ${quantity}개`)
+        .join('\n')}
 
-      ${this.viewBenefits(benefit, visitDate)}
-
-      ${TITLE.PAYMENT}
-      ${this.numberFormat(totalPrice - benefit.totalBenefitPrice())}원
-
-      ${TITLE.BADGE}
-      ${Membership.badge(benefit.totalBenefitPrice()) || '없음'}`.replace(/(\n\s+)/g, '\n');
+      ${TITLE.TOTAL_PRICE}
+      ${formatter.numberFormat(totalPrice)}원
+    `;
   },
-  printReceipt(totalPrice, orderDetail, visitDate, benefit) {
-    const receipt = this.createRecipt(totalPrice, orderDetail, visitDate, benefit);
+
+  createRecipt(totalPrice, orders, visitDate, benefit) {
+    const orderDetail = this.viewOrderDetail(totalPrice, orders);
+    const benefitDetail = this.viewBenefitDetail(benefit, visitDate);
+    const badge = Membership.badge(benefit.totalBenefitPrice()) || '없음';
+    const payment = formatter.numberFormat(totalPrice - benefit.totalBenefitPrice());
+    const receipt = [orderDetail, benefitDetail, TITLE.PAYMENT, payment, TITLE.BADGE, badge];
+
+    return receipt.join('\n')
+      .replace(/(\n\s+)/g, '\n');
+  },
+
+  printReceipt(totalPrice, orders, visitDate, benefit) {
+    const receipt = this.createRecipt(totalPrice, orders, visitDate, benefit);
 
     Console.print(receipt);
   },
